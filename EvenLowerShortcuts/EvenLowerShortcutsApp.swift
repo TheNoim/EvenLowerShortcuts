6cb79dbb55f4494b56c75f4b641f04a2f4993a11
Nils Bergmann <nilsbergmann@noim.io>
2023-01-30 17:47:40 +0100

    Initial Commit

---
+++ EvenLowerShortcuts/EvenLowerShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvenLowerShortcutsApp.swift
+//  EvenLowerShortcuts
+//
+//  Created by Nils Bergmann on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct EvenLowerShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
